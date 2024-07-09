3bc04c246ba047a2e4a7be6607acf5c543eea31c
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-07-08 22:03:55 -0300

    test: add new mock data

---
+++ __tests__/ui/band.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import BandComponent from "@/pages/bands/[bandId]";
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+
+test("band component displays correct band information", async () => {
+  const { fakeBands } = await readFakeData();
+  render(<BandComponent band={fakeBands[0]} error={null} />);
+
+  const heading = screen.getByRole("heading", {
+    name: /the wandering bunnies/i,
+  });
+
+  expect(heading).toBeInTheDocument();
+});
--- __tests__/ui/home.test.tsx
+++ __tests__/ui/home.test.tsx
@@ -5,9 +5,11 @@ import Home from "@/pages/index";
 test("page has correct heading and image", () => {
   render(<Home />);
 
-  const heading = screen.getByRole("heading", {
-    name: "Welcome to Popular Concert Venue",
-  });
+  const heading = screen.getByRole("heading", { name: "Welcome to Popular Concert Venue" });
+  const image = screen.getByRole("img", { name: "Concert goer with hands in the shape of a heart" });
 
   expect(heading).toBeInTheDocument();
+  expect(image).toBeInTheDocument();
+
+  //More tests here
 });
