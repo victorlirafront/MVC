566bab5718238d35b09bc88f5cac8f0561325faa
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-07-16 22:34:50 -0300

    test: reservation page shows 'sold out' message and NO purchase button if there are no seats availabe

--- __tests__/__mocks__/msw/handlers.js
+++ __tests__/__mocks__/msw/handlers.js
@@ -8,10 +8,13 @@ export const handlers = [
   //rest é um objeto fornecido para biblioteca msw
   rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
     const { fakeShows } = await readFakeData();
+    const { showId } = req.params;
 
+    //index / showId = 0 has seats available in fake data
+    //index / showId = 1 has No seats available
     return res(
       ctx.json({
-        show: fakeShows[0],
+        show: fakeShows[Number(showId)],
       })
     );
   }),
