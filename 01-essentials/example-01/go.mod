c323954df6a0fcdcf2884e39b15b7b994d76d886
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-07 01:42:09 -0300

    feat: add example 1

---
+++ 01-essentials/example-01/go.mod
@@ -0,0 +1,3 @@
+module example/investment-calculator
+
+go 1.23.4
---
+++ 01-essentials/example-01/investment_calculator.go
@@ -0,0 +1,10 @@
+package main
+
+// go will execute this function when the program starts
+func main() {
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = investmentAmount * (1 + expectedReturnRate/100)
+}
