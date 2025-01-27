bc9900c3a3e8c2bb3fbca5a5d0612709454daadc
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-27 19:04:59 -0300

    feat: add investmentAmount

--- 01-essentials/example-01/investment_calculator.go
+++ 01-essentials/example-01/investment_calculator.go
@@ -1,10 +1,19 @@
 package main
 
-// go will execute this function when the program starts
-func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+import (
+	"fmt"
+	"math"
+)
 
-	var futureValue = investmentAmount * (1 + expectedReturnRate/100)
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64 = 1000
+	expectedReturnRate := 5.5
+	var years = 10.0
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
