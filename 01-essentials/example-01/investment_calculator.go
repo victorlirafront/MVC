2687c07dd406e9b12893b802f5b1b31f873bb1cf
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-29 19:54:02 -0300

    feat: add fmt.Scan

--- 01-essentials/example-01/investment_calculator.go
+++ 01-essentials/example-01/investment_calculator.go
@@ -7,9 +7,18 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
-	expectedReturnRate := 5.5
-	var years = 10.0
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Investment Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
