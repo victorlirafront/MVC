13aa9db831e700965b3fd93c2786d359bc5e9333
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-30 19:46:33 -0300

    feat: add a new method for string concatenation

--- 01-essentials/example-01/investment_calculator.go
+++ 01-essentials/example-01/investment_calculator.go
@@ -23,6 +23,10 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	//outputs information
+
+	//fmt.Println("Future Value:", futureValue)
+
+	fmt.Printf("Future Value: %v\nFuture Value (adjusted for inflation): %v", futureValue, futureRealValue)
+	// fmt.Println("Future Value (adjusted for inflation):", futureRealValue)
 }
