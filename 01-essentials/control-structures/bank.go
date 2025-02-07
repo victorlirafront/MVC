3c701dd4e9b6e710865b9acb096b0ea193c23dc5
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-06 21:58:12 -0300

    feat: add accountBalance func

---
+++ 01-essentials/control-structures/bank.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var accountBalance float64 = 1000
+
+	fmt.Println("Welcome go Go Bank")
+	fmt.Println("What do you want to do ?")
+	fmt.Println("1. Check the balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	// wantsCheckBalance := choice == 1
+
+	if choice == 1 {
+		fmt.Println("Your balance is", accountBalance)
+	}
+
+	fmt.Println("Your choice:", choice)
+}
