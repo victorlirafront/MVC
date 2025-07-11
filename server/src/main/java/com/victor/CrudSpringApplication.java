0768be9f4c253ed723bca754ebc608c65bf3585d
victorlirafront <victorliradeveloper@gmail.com>
2025-07-11 13:46:16 -0300

    feat: add CourseController

--- server/src/main/java/com/victor/CrudSpringApplication.java
+++ server/src/main/java/com/victor/CrudSpringApplication.java
@@ -1,13 +1,32 @@
 package com.victor;
 
+import com.victor.model.Course;
+import com.victor.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	//o codigo abaixo é apenas para exemplo, isso nao iria para produção : )
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository){
+		return args -> {
+
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
 	}
 
 }
